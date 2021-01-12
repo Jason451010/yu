0c516eede8a4886899ce4708e6e5543cedbe50ee
Jason451010 <63780754+Jason451010@users.noreply.github.com>
2021-01-12 21:28:31 +0800

    Create 13.2head.php

---
+++ 13.2head.php
@@ -0,0 +1,6 @@
+< html >
+    <頭>
+        < title >佈告管理介面</ title >
+    </頭>
+    <身體>
+    < div 樣式= “高度：70px； text-align：center； background-color：灰色； line-height：70px ” > < h1 >佈告管理介面</ h1 > </ div >
