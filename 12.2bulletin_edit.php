099b79cef60cebc37325e2dda9f941f0628bc742
Jason451010 <63780754+Jason451010@users.noreply.github.com>
2021-01-12 21:26:53 +0800

    Create 12.2bulletin_edit.php

---
+++ 12.2bulletin_edit.php
@@ -0,0 +1,20 @@
+﻿<？php
+    session_start（）;
+if（！isset（$ _SESSION [ 'id' ]））{   
+    迴聲 “請登錄系統”；
+    echo  “ <元http-equiv ='refresh'content ='3; url = index.html''>” ;
+}其他{
+    $ conn = mysqli_connect（ “ localhost”， “ root”， “”， “ mydb”）;
+    如果（mysqli_connect_error（$ conn））
+      die（“無法連線資料庫”）;
+    $ sql = “更新公告集title ='{$ _ POST ['title']}'，content ='{$ _ POST ['content']}'，type = {$$ POST ['type']}，time =' {$ _POST ['time']}'其中bid = {$ _ POST ['bid']}“”；
+    // echo $ sql;
+    如果（！mysqli_query（$ conn，$ sql））{
+     echo（“錯誤描述：”。mysqli_error（$ conn））;   
+    }
+    其他  
+       迴聲 “修改成功”；   
+    mysqli_close（$ conn）;
+    echo  “ <元http-equiv ='refresh'content ='3; url = bulletin.php''>” ;
+}
+？>
